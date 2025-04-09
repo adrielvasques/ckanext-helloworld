cb10f4ac77a6981ec8ed6e7f32b3a4e2063a8792
Adriel Santos <adriel.vasques@hotmail.com.br>
2025-04-09 05:11:52 -0400

    Adicionando suporte minimo

--- ckanext/helloworld/controller.py
+++ ckanext/helloworld/controller.py
@@ -1,5 +1,13 @@
-from ckan.lib.base import BaseController
+from flask import Blueprint, render_template, abort
 
-class HelloWorldController(BaseController):
-    def dispatch_get(self, *args, **kwargs):
-        return "Hello World! Here are your %s" % kwargs['eggs']
+helloworld_blueprint = Blueprint('helloworld', __name__, url_prefix='/hello')
+
+@helloworld_blueprint.route('/world/<eggs>', methods=['GET'])
+def hello_world(eggs):
+    """A simple hello world page."""
+    return f"Hello World! You passed: {eggs}"
+
+# If you had more complex logic or wanted to use templates, you could do something like:
+# @helloworld_blueprint.route('/template/<name>', methods=['GET'])
+# def hello_template(name):
+#     return render_template('helloworld/hello.html', name=name)
