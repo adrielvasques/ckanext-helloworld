4ba3cad4eb8dd962c0b47deb01ef0a8880fb7d3c
Adriel Santos <adriel.vasques@hotmail.com.br>
2025-04-09 05:16:53 -0400

    Errei

--- ckanext/helloworld/__init__.py
+++ ckanext/helloworld/__init__.py
@@ -1,10 +1,7 @@
-import ckan.plugins as p
-from .controller import helloworld_blueprint
-
-class HelloWorldPlugin(p.SingletonPlugin):
-    """Plugin that adds a new URL to the site to show some text."""
-    p.implements(p.IBlueprint)
-
-    def get_blueprint(self):
-        """Return a Flask Blueprint object to define routes."""
-        return helloworld_blueprint
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
